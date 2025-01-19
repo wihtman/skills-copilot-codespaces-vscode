f6c77aedc005e9fb40279b323dde99c52e78b34d
Wihtman Quintero <wihtman@gmail.com>
2025-01-19 14:38:32 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,17 @@
+function skillsMember() {
+    return {
+        name: 'Member',
+        skills: [
+            'Javascript',
+            'HTML',
+            'CSS',
+            'React',
+            'Angular',
+            'Vue',
+            'Node',
+            'Express',
+            'MongoDB',
+            'SQL'
+        ]
+    }
+}
